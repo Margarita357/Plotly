36a7854c4c4e7ce600792d7de580b7282864ee82
Margarita357 <37559337+Margarita357@users.noreply.github.com>
2018-10-08 23:10:06 -0700

    updated

---
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -0,0 +1,90 @@
+function buildMetadata(sample) {
+
+  // @TODO: Complete the following function that builds the metadata panel
+  d3.json(`/metadata/${sample}`).then((data) => {
+    var panel = d3.select("#sample-metadata");
+    // Use `.html("") to clear any existing metadata
+    panel.html("");
+  
+    Object.entries(data).forEach(([key, value]) => {
+      panel.append("h6").text(`${key}: ${value}`);
+    });
+ 
+    // BONUS: Build the Gauge Chart
+    // buildGauge(data.WFREQ);
+    buildGauge(data.WFREQ);
+  });
+ }
+ 
+ function buildCharts(sample) {
+  d3.json(`/samples/${sample}`).then((data) => {
+    const otu_ids = data.otu_ids;
+    const otu_labels = data.otu_labels;
+    const sample_values = data.sample_values;s
+
+    var bubbleLayout = {
+      margin: { t: 0 },
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" }
+    };
+    var bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "Earth"
+        }
+      }
+    ];
+    Plotly.plot("bubble", bubbleData, bubbleLayout);
+
+    var pieData = [
+      {
+        values: sample_values.slice(0, 10),
+        labels: otu_ids.slice(0, 10),
+        hovertext: otu_labels.slice(0, 10),
+        hoverinfo: "hovertext",
+        type: "pie"
+      }
+    ];
+ 
+    var pieLayout = {
+      margin: { t: 0, l: 0 }
+    };
+ 
+    Plotly.plot("pie", pieData, pieLayout);
+  });
+ }
+ 
+ function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+ 
+  // Use the list of sample names to populate the select options
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+ 
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+ }
+ 
+ function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+ }
+ 
+ // Initialize the dashboard
+ init();
